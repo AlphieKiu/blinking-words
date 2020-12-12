8b083fdfc994fb48da7d82af0ff27d8b9a6f0e61
AlphieKiu <kiuhuaien87@gmail.com>
2020-12-12 12:13:10 -0600

    set up with data.js file, added bootstrap to styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { data } from '../assets/data.js'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blinking-words';
+  text = data.text;
 }
